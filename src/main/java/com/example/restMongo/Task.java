8781b684ff1323db22718a9767468ce1c21003e4
Stuart <stuartb@live.com>
2020-05-12 13:04:20 +0100

    get and set in Task.java

---
+++ src/main/java/com/example/restMongo/Task.java
@@ -0,0 +1,77 @@
+package com.example.restMongo;
+
+import java.util.Date;
+
+public class Task {
+	private Long id;
+	private String subject;
+	private String description;
+	private Date created;
+	private Date updated;
+	private Date due;
+	private boolean important;
+
+	public Task(String subject, String description, Date due, boolean important) {
+		this.subject = subject;
+		this.description = description;
+		this.due = due;
+		this.important = important;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	public Date getDue() {
+		return due;
+	}
+
+	public void setDue(Date due) {
+		this.due = due;
+	}
+
+	public boolean isImportant() {
+		return important;
+	}
+
+	public void setImportant(boolean important) {
+		this.important = important;
+		updated = new Date();
+	}
+}
