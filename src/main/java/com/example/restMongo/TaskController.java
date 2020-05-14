db70c113180d12cd9e16d16e360cb1922f615b81
Stuart <stuartb@live.com>
2020-05-14 12:42:50 +0100

    CORS

--- src/main/java/com/example/restMongo/TaskController.java
+++ src/main/java/com/example/restMongo/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.time.LocalDateTime;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 public class TaskController {
     @Autowired
